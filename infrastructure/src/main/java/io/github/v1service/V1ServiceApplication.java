3462304fef5719d820b447d2576281ef7deccd64
parksangwoo <b01056721617@gmail.com>
2022-07-05 19:23:36 +0900

    📑 :: (#1) setting package structure, dependencies

---
+++ infrastructure/src/main/java/io/github/v1service/V1ServiceApplication.java
@@ -0,0 +1,11 @@
+package io.github.v1service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(V1ServiceApplication.class, args);
+    }
+}
